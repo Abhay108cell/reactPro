5e7466c51e5d898fd0c51a8aceec96053ea5c91e
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-08 19:14:20 +0530

    init chatFeed.jsx

--- chat-App/src/App.jsx
+++ chat-App/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import {ChatEngine} from "react-chat-engine";
 import './App.css'
+import ChatFeed from './component/chatFeed.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
