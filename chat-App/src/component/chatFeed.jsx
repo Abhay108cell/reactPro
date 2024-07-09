26f11b7bd5f5ef3318dcf0b26ff1eb0d84d5eb27
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-09 22:20:49 +0530

    rendering messages in chatFeed.jsx

--- chat-App/src/component/chatFeed.jsx
+++ chat-App/src/component/chatFeed.jsx
@@ -1,4 +1,20 @@
+ import MessagesForm from "./MessagesForm"
+ import MyMessages from "./MyMessages"
+ import TheirMessages from "./TheirMessages"
+
+
+
  const ChatFeed = ()=>{
+  const {chats, activeChat, userName, messages} = props;
+  const chat = chats && chats[activeChat];
+  const renderMessages = () => {
+    const keys = Object.keys(messages)
+    return keys.map((key, index)=>{
+      const message = messages[key]
+      const lastMessageKey = index === 0? null: keys [index-1];
+      const isMyMessage = userName === message.sender.userName
+    })
+  }
   return(
     <div>
       chat feed
