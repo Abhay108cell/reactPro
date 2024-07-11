f6378f45609d95e34fd3c3de4a68d532ad95c8b2
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-11 17:36:28 +0530

    message-avater shwoing TheirMessages.jsx

--- chat-App/src/component/TheirMessages.jsx
+++ chat-App/src/component/TheirMessages.jsx
@@ -1,7 +1,12 @@
 const TheirMessages =({lastMessage, message})=>{
+  const isFirstMessageByUser =!lastMessage || lastMessage.sender.username !==  message.sender.username;
+
   return(
-    <div>
-    <h1>Their Messages</h1>
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div className="message-avater"
+        style={{backgroundImage: message.sender && `url(${message.sender.avatar})`}}></div>
+      )}
     </div>
   )
 }
