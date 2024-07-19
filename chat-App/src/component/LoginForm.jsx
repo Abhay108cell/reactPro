f029e92d45cd4a7d70dbe174c1786a45af16b534
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-19 19:05:27 +0530

    bug fixed in App.jsx

--- chat-App/src/component/LoginForm.jsx
+++ chat-App/src/component/LoginForm.jsx
@@ -37,7 +37,7 @@ const ProjectId = "692aa9c2-d73e-4086-8798-4b1eded86b08"
           <div className="form">
             <h1 className="title">Chat Application</h1>
             <form onSubmit={handleSubmit}>
-              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="userName" required />
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
               <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="password" required />
               <div align="center">
                 <button className="button">
