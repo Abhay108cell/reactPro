b4f1a3b67b7e09107aa68c20fe7a6922af5ad4f6
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-22 22:05:28 +0530

    found a bug  MovieList.jsx

--- movieApp/src/App.jsx
+++ movieApp/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import './App.css';
+import MovieList from './components/MovieList';
 
 function App() {
   const [movies, setMovies] = useState([
