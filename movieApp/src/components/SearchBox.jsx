2d74de49daea4e79459334e347320ecae30f963e
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-23 19:03:19 +0530

    completed SearchBox.jsx

--- movieApp/src/components/SearchBox.jsx
+++ movieApp/src/components/SearchBox.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const SearchBox =(props)=>{
   return(
     <div className="col col-sm-4">
-      <input type="text" />
+      <input 
+      className="form-control"
+      value={props.value}
+      onChange={(event)=> props.setSearchValue(event.target.value)}
+      placeholder="Type to Search..."
+       />
     </div>
   )
 }
