d890c10bf5770c88d512b5a03524230e533b5291
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-30 18:34:23 +0530

    Grid styling in App.jsx

--- videoSharing/src/App.jsx
+++ videoSharing/src/App.jsx
@@ -4,9 +4,21 @@ import {Grid} from "@material-ui/core"
 
 function App() {
   return (
-   <div className="App">
-    <h1>Video Sharing Application</h1>
-   </div>
+   <Grid style={{justifyContent :  "centre"}} container spacing={10}>
+    <Grid item xs={11}>
+      <Grid container spacing={10}>
+        <Grid item xs={12} >
+          {/* {SearchBar} */}
+        </Grid>
+        <Grid item xs={8}>
+          {/* {VideoDetails} */}
+        </Grid>
+        <Grid item xs={4}>
+          {/* {VideoList} */}
+        </Grid>
+      </Grid>
+    </Grid>
+   </Grid>
   )
 }
 
