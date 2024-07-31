1aeb2459c6d568fdab976843c86be29db64d1611
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-31 16:34:54 +0530

    using event in handleChange onKeyPress function in SearchBar.jsx

--- videoSharing/src/Components/SearchBar.jsx
+++ videoSharing/src/Components/SearchBar.jsx
@@ -3,12 +3,24 @@ import {Paper, TextField} from "@material-ui/core"
 
 const SearchBar =() =>{
   const [searchTerm, setSearchTerm] = useState('')
+
+  const handleChange = (event)=> setSearchTerm(event.target.value)
+
+  const onKeyPress = (event)=>{
+    if(event.key === "Enter"){
+      onsubmit(searchItem)
+    }
+  }
+  
+
   return(
     <Paper elevation={6}  styling={{padding : "25px" }}>
       <TextField 
       label="Search..."
       fullWidth
       value={searchTerm}
+      onChange={handleChange}
+      onKeyPress={onKeyPress}
         />
     </Paper>
   )
