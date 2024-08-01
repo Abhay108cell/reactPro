328f05d8dce8e56e325f657b698cdd8b9a556da0
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-01 16:09:26 +0530

    using materialUI comp. in VideoDetails.jsx

--- videoSharing/src/Components/VideoDetails.jsx
+++ videoSharing/src/Components/VideoDetails.jsx
@@ -1,5 +1,12 @@
 import { Paper, Typography } from '@material-ui/core';
 
 const VideoDetails = ({video: {id:{videoId}, snippet:{title, channelTitle, description}}})=>{
+   if (!videoId) return <div>Loading...</div>
+   return(
+   < React.Fragment>
+    <Paper elevation={6} style={{}}>
 
+    </Paper>
+   </ React.Fragment>
+   )
 }
