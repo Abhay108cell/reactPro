0c5eede67485874db05f2f5bff6b95f4c16e5ff2
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-02 20:03:37 +0530

    using map function for VideoItem videoLists.Jsx

--- videoSharing/src/VideoLists.jsx
+++ videoSharing/src/VideoLists.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import Grid from '@mui/material/Grid';
 
 const VideoLists = ({videos ,onVideoSelect}) =>{
+  const ListOfVideos = videos.map(video=>{
+    <VideoItem
+    onVideoSelect={onVideoSelect}
+    key={video.id.videoId}
+    video={video}
+    />
+  })
   return(
     <Grid container spacing={10}>
       {ListOfVideos}
