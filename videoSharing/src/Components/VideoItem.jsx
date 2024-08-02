6bf04fedf54e6da7b0cda7d7bb25d88803df7c8d
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-02 21:56:36 +0530

    using Typography comp. in videoItem.Jsx

--- videoSharing/src/Components/VideoItem.jsx
+++ videoSharing/src/Components/VideoItem.jsx
@@ -7,7 +7,11 @@ const VideoItem = () => {
   <Grid item xs={12}>
     <Paper
       style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-      onClick={() => onVideoSelect(Video)}
-    ></Paper>
+      onClick={() => onVideoSelect(Video)}>
+        <img style={{marginRight: '20px' }} alt="thumbnail" src={video.snippet.thumbnails.medium.url} />
+        <Typography variant='subtitle1'>
+          <b>{video.snippet.title}</b>
+        </Typography>
+      </Paper>
   </Grid>;
 };
