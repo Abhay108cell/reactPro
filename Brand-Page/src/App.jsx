21b7c659b78d471ce4de46c5b200b2aa8f1691dd
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-03 18:11:51 +0530

    init App.jsx for project

--- Brand-Page/src/App.jsx
+++ Brand-Page/src/App.jsx
@@ -10,6 +10,12 @@ function App() {
       <div className="logo">
         <img src="/images/brand_logo.png" alt="" />
       </div>
+      <ul>
+        <li>MENU</li>
+        <li>LOCATION</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+      </ul>
     </nav>
     </>
   )
