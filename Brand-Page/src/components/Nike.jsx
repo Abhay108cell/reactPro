e27de291ab8b97f6140411680cb162550a27847b
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-04 19:54:59 +0530

    content add in Nike.jsx

--- Brand-Page/src/components/Nike.jsx
+++ Brand-Page/src/components/Nike.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Nike=()=> {
   return (
-    <div>
-      
+   <main className="nike">
+    <div className="nike-content">
+      <h1>YOUR FEET DESERVE THE BEST</h1>
+      <p>
+      YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+      </p>
+      <div className="nike-btn">
+        <button>SHOP NOW</button>
+        <button>Category</button>
+      </div>
+      <div className="shopping">
+        <p>Also Available On</p>
+        
+      </div>
     </div>
+    <div className="nike-image"></div>
+   </main>
   )
 }
 
