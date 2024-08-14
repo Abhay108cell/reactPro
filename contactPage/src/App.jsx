703df3552922ff5e3f0b4de28ec326ca7a8be9ce
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-14 22:17:57 +0530

    created Navigation Component and CSS module

--- contactPage/src/App.jsx
+++ contactPage/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react' 
 import './App.css'
-import NavBar from './components/NavBar'
+import NavBar from './components/Navigation/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
