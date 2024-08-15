bfe7e9be3c5e1fe87ecbc20504b59e0b4e4531b2
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-15 18:07:49 +0530

    created comp and file Button and init it

--- contactPage/src/components/contactFrom/ContactForm.jsx
+++ contactPage/src/components/contactFrom/ContactForm.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import styles from './ContactForm.module.css'
 
 const ContactForm = () => {
-  return <section className={styles.container}></section>
+  return <section className={styles.container}>
+    <div className={styles.contact_form}></div>
+    <div className={styles.contact_image}></div>
+  </section>
 }
 
 export default ContactForm
