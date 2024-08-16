b77ea2579a793b0d27087c9515a29deabbbbebe1
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-16 16:40:50 +0530

    using props in contactForm and passed to button component

--- contactPage/src/components/contactForm/ContactForm.jsx
+++ contactPage/src/components/contactForm/ContactForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './ContactForm.module.css'
 import Button from '../button/Button'
+import { MdMessage } from "react-icons/md";
 
 const ContactForm = () => {
   return <section className={styles.container}>
     <div className={styles.contact_form}>
-     < Button/>
+     < Button text='VIA SUPPORT CHAT' icon={<MdMessage fontSize='24px'/>}/>
+     < Button text='VIA SUPPORT CHAT' icon={<MdMessage fontSize='24px'/>}/>
     </div>
     <div className={styles.contact_image}></div>
   </section>
