372d1aa9fa3fff8568c882c7a9e8d87409a0d2b8
abhay kumar <abhaykumar2871999@gmail.com>
2024-08-16 16:28:58 +0530

    init Button.module.css and import it in Button.jsx

--- contactPage/src/components/button/Button.jsx
+++ contactPage/src/components/button/Button.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { MdMessage } from "react-icons/md";
+import styles from './Button.module.css'
 
 
 const Button = () => {
   return (
-    <div>
+    <button className={styles.primary_btn}>
       <MdMessage />
-
-    </div>
+      VIA SUPPORT CHAT
+    </button>
   )
 }
 
