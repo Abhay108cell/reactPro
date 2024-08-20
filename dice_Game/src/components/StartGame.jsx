b66e4046f9839938878e4622a74b0d2dc9f19be0
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-20 17:51:03 +0530

    "Updated StartGame component: added className to div, styled h1 and button elements, and removed trailing newline"

--- dice_Game/src/components/StartGame.jsx
+++ dice_Game/src/components/StartGame.jsx
@@ -4,35 +4,39 @@ const StartGame = () => {
   return (
     <Container>
       <div>
-      <img src="/images/dices.png" alt=""  />
+        <img src="/images/dices.png" alt="" />
       </div>
-      <div>
+      <div className="content">
         <h1>DICE GAME</h1>
         <Button>Play Now</Button>
       </div>
     </Container>
-  )
-  
-  
-}
+  );
+};
 
 export default StartGame;
 
- const Container = styled.div`
+const Container = styled.div`
   max-width: 1180px;
   margin: 0 auto;
   display: flex;
   height: 100vh;
   align-items: center;
- `;
 
+  .content {
+    h1 {
+      font-size: 96px;
+      white-space: nowrap;
+    }
+  }
+`;
 
- const Button = styled.button`
+const Button = styled.button`
   color: white;
   padding: 10px 18px;
   background: #000;
-  border-radius : 5px;
+  border-radius: 5px;
   min-width: 220px;
   border: none;
-
- `
+  font-size: 16px;
+`;
