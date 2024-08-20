924264a87a8c680add10b310290434eaf56f6b99
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-20 18:10:24 +0530

    "Added useState hook and toggle function to App component"

--- dice_Game/src/App.jsx
+++ dice_Game/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import StartGame from './components/StartGame'
 // import styled from "styled-components";
 
 function App() {
 
+  const [isGameStarted, setIsGameStarted] = useState(false)
+
+  const toogleGamePlay = () =>{
+    setIsGameStarted((prev)=> !prev);
+  }
   
   
   return (
