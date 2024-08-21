9a3c4790a60b0f7aa836383b24aed3394291990e
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-21 17:44:03 +0530

    "Added NumberSelector component to GamePlay component and implemented basic styling for NumberSelector"

--- dice_Game/src/components/NumberSelector.jsx
+++ dice_Game/src/components/NumberSelector.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from "styled-components";
+
 
 const NumberSelector = () => {
   return (
     <div>
-      
+      <Box>1</Box>
     </div>
   )
 }
 
-export default NumberSelector
+export default NumberSelector;
+
+const Box = styled.div`
+  height:72px;
+  width:72px;
+  border: 1px solid black;
+  display: grid;
+  place-items: center;
+  font-size: 24px;
+  font-weight:700;
+  
+`
