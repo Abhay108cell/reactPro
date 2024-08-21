a5987280ded52a00acf38cb4ec56d09e8bb9a203
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-21 16:45:10 +0530

    "Updated App.jsx to initialize isGameStarted as true, and modified TotalScore.jsx to add text alignment and max width to ScoreContainer, and adjust font styles for h1 and p elements."

--- dice_Game/src/components/TotalScore.jsx
+++ dice_Game/src/components/TotalScore.jsx
@@ -14,8 +14,11 @@ const TotalScore = () => {
 export default TotalScore
 
 const ScoreContainer = styled.div`
+text-align: center;
+max-width:200px;
   h1{
     font-size: 100px;
+    line-height:100px
   }
   p{
     font-size: 24px;
