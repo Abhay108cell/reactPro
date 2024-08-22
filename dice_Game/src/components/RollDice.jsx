0ffb320acb344e65ad61829186000bccd1840874
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-08-22 18:32:08 +0530

    "Added useState hook and generateRandomeNumber function to RollDice component, updated CSS for DiceContainer and added cursor pointer to .dice class"

--- dice_Game/src/components/RollDice.jsx
+++ dice_Game/src/components/RollDice.jsx
@@ -1,10 +1,18 @@
-
+import { useState } from "react";
 import styled from "styled-components";
 
 const RollDice = () => {
+
+  const [currentDice, setCurrentDice] = useState();
+
+  const generateRandomeNumber = (min, max) =>{
+    return Math.random() * (max - min)+ min;
+  }
+
   return (
     <DiceContainer>
-      <div>
+      <div className="dice"
+      onClick={() =>generateRandomeNumber(1,6)} >
         <img src="/images/dice/dice_1.png" alt="dice 1" />
       </div>
       <p>Click on Dice to roll</p>
@@ -19,6 +27,11 @@ display: flex;
 margin-top: 48px;
 flex-direction: column;
 align-items: center; 
+
+.dice{
+cursor: pointer;
+}
+
 p{
   font-size: 24px;
 }
