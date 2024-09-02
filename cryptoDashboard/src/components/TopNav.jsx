8689e9f2463a14ed403e5c8e2bc3fcaaea37c400
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-02 18:08:11 +0530

    "Updated TopNav component: added Icon to MenuButton, changed Heading font size, and reformatted code"

--- cryptoDashboard/src/components/TopNav.jsx
+++ cryptoDashboard/src/components/TopNav.jsx
@@ -1,29 +1,44 @@
-import {  Heading,Menu, MenuButton,MenuList, MenuItem, Button, HStack, Box } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Heading,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  HStack,
+  Box,
+  Icon,
+} from "@chakra-ui/react";
+import React from "react";
+import { FaCircleUser } from "react-icons/fa6";
 
 const TopNav = () => {
   return (
-    <Box>
-
-   <HStack maxWidth="60rem" mx="auto" boxShadow="xl" h="16px" justify="space-between" px="32">
-        
-          <Heading >Dashboard </Heading>
-          <Menu>
-  <MenuButton as={Button}>
-    Actions
-  </MenuButton>
-  <MenuList>
-    <MenuItem>Download</MenuItem>
-    <MenuItem>Create a Copy</MenuItem>
-    <MenuItem>Mark as Draft</MenuItem>
-    <MenuItem>Delete</MenuItem>
-    <MenuItem>Attend a Workshop</MenuItem>
-  </MenuList>
-</Menu>
-        
-   </HStack>
+    <Box boxShadow="xl">
+      <HStack
+        maxWidth="60rem"
+        mx="auto"
+        h="16px"
+        justify="space-between"
+        px="32"
+      >
+        <Heading fontSize="28px">Dashboard </Heading>
+        <Menu>
+          <MenuButton as={Button}>
+            <Icon
+            as={FaCircleUser}           />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+      </HStack>
     </Box>
-  )
-}
+  );
+};
 
-export default TopNav
+export default TopNav;
