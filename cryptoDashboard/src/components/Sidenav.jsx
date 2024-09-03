33952e8f084e8ff572192fe6c233079b032f59b1
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-03 22:01:54 +0530

    "Refactored DashboardLayout and Sidenav components: updated JSX syntax, added parentheses to useDisclosure, and modified Stack component props in Sidenav."

--- cryptoDashboard/src/components/Sidenav.jsx
+++ cryptoDashboard/src/components/Sidenav.jsx
@@ -19,47 +19,54 @@ const Sidenav = () => {
   ];
 
   return (
-    <Stack justifyContent="space-between" boxShadow="lg" w="16rem" h="100vh"
-    
-     >
+    <Stack justifyContent="space-between" 
+    boxShadow={{
+      base: "none",
+      lg: "lg", 
+    }}
+     w={{
+      base: "full",
+      lg: "16rem",
+     }}
+      h="100vh">
       <Box>
-
-      <Heading textAlign="center" fontSize="20px" pt="3.5rem" as="h1">
-        Crypto Project
-      </Heading>
-      <Box mt="6" mx="3">
-        {navLink.map((nav) => (
-          <HStack  
-          borderRadius="10px"
-          key={nav.text} 
-          py="3" 
-           px="4"
-           _hover={{
-            bg: "#f3f3f7",
-            color:"#171717",
-           }}
-           color="#797E82"
-          >
-            <Icon as={nav.icon} />
-            <Text fontWeight="medium" fontSize="14px">{nav.text}</Text>
-          </HStack>
-        ))}
-      </Box>
+        <Heading textAlign="center" fontSize="20px" pt="3.5rem" as="h1">
+          Crypto Project
+        </Heading>
+        <Box mt="6" mx="3">
+          {navLink.map((nav) => (
+            <HStack
+              borderRadius="10px"
+              key={nav.text}
+              py="3"
+              px="4"
+              _hover={{
+                bg: "#f3f3f7",
+                color: "#171717",
+              }}
+              color="#797E82"
+            >
+              <Icon as={nav.icon} />
+              <Text fontWeight="medium" fontSize="14px">
+                {nav.text}
+              </Text>
+            </HStack>
+          ))}
+        </Box>
       </Box>
 
       <Box mt="6" mx="3" mb="6">
-      <HStack  
+        <HStack
           borderRadius="10px"
-          py="3" 
-           px="4"
-           _hover={{
+          py="3"
+          px="4"
+          _hover={{
             bg: "#f3f3f7",
-           }}
-          >
-            <Icon as={BiSupport} />
-            <Text fontSize="14px">Support</Text>
-          </HStack>
-
+          }}
+        >
+          <Icon as={BiSupport} />
+          <Text fontSize="14px">Support</Text>
+        </HStack>
       </Box>
     </Stack>
   );
