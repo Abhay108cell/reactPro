7420931494f915dda588c923c7d15ae26a33b668
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-04 22:11:11 +0530

    "Updated PortfolioSection component with minor formatting changes and added new Stack component for Wallet Balance"

--- cryptoDashboard/src/pages/Dashboard/components/PortfolioSection.jsx
+++ cryptoDashboard/src/pages/Dashboard/components/PortfolioSection.jsx
@@ -1,6 +1,6 @@
-import { HStack, Stack,Icon, Text } from '@chakra-ui/react'
-import React from 'react'
-import { AiOutlineInfoCircle } from 'react-icons/ai'
+import { HStack, Stack, Icon, Text } from "@chakra-ui/react";
+import React from "react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const PortfolioSection = () => {
   return (
@@ -8,14 +8,25 @@ const PortfolioSection = () => {
       <Stack>
         <HStack color="black.80">
           <Text fontSize="sm">Total Portfolio Value</Text>
-        <Icon
-        as={AiOutlineInfoCircle}
-        /> 
+          <Icon as={AiOutlineInfoCircle} />
         </HStack>
         <Text textStyle="h2">$122,23.54 </Text>
       </Stack>
+      <Stack>
+        <HStack color="black.80">
+          <Text fontSize="sm">Wallet Balance</Text>
+        </HStack>
+        <HStack>
+          <HStack>
+            <Text textStyle="h2">22.39401000 </Text> <Tag>BTC</Tag>
+          </HStack>
+          <HStack>
+            <Text textStyle="h2">1300.00 </Text> <Tag>Inr</Tag>
+          </HStack>
+        </HStack>
+      </Stack>
     </HStack>
-  )
-}
+  );
+};
 
-export default PortfolioSection
+export default PortfolioSection;
