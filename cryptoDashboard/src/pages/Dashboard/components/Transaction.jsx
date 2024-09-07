ff575e35f8dc69ef9f6de2cb0f687248305a4f05
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-07 17:37:55 +0530

    " Added Array of Sample data and Added transaction data and mapping to Transaction component"

--- cryptoDashboard/src/pages/Dashboard/components/Transaction.jsx
+++ cryptoDashboard/src/pages/Dashboard/components/Transaction.jsx
@@ -11,9 +11,41 @@ import {
 } from "@chakra-ui/react";
 
 const Transaction = () => {
+
+  const transaction =[
+    {
+      id: 1,
+      icon: "",
+      text: "INR Deposit",
+      amount: "+$81,1000",
+      date: "2022-01-01 7:06 PM",
+      },
+    {
+      id: 2,
+      icon: "",
+      text: "BTC Sell",
+      amount: "-$8110.20",
+      date: "2022-01-01 8:06 PM",
+      },
+    {
+      id: 3,
+      icon: "",
+      text: "INR Deposit",
+      amount: "+$95,5000",
+      date: "2022-01-01 11:06 PM",
+      },
+  ]
+
   return (
     <CustomCard>
       <Text fontSize="sm" color="black.80">Recent Transaction</Text>
+      <Stack direction="column" spacing={4}>
+        {transaction.map((transaction) => (
+          <Flex key={transaction.id}>
+
+          </Flex>
+        ))}
+      </Stack>
     </CustomCard>
   )
 }
