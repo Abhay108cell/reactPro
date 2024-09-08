3c51f33209cdd55e2e4597a86e698ffd1cab7f3f
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-08 21:40:48 +0530

    "Added 'inverted' prop to InfoCard component, updated styles to support inverted mode"

--- cryptoDashboard/src/pages/Dashboard/components/InfoCard.jsx
+++ cryptoDashboard/src/pages/Dashboard/components/InfoCard.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import { CustomCard } from '../../../chakra/CustomCard'
 import { Tag, Text } from '@chakra-ui/react'
 
-const InfoCard = ({imgURL, text, tagText}) => {
+const InfoCard = ({imgURL, text, tagText, inverted}) => {
   return (
-    <CustomCard bgImg={imgURL} bgSize="cover" bgRepeat="no-repeat">
+    <CustomCard bgImg={imgURL} bgSize="cover" bgRepeat="no-repeat"
+    bg={inverted ? "p.purple" : "white" }
+    >
 
-      <Tag bg="p.purple" color="white" borderRadius="full">{tagText}</Tag>
-      <Text mt="4" fontWeight="medium" textStyle="h5">
+      <Tag 
+      color={inverted ? "p.purple" : "white"}
+      bg={inverted ? "white" : "p.purple"}
+      borderRadius="full"
+      >{tagText}</Tag>
+      <Text mt="4" fontWeight="medium" textStyle="h5"
+        color={inverted ? "white" : "black.80"}
+       
+       >
         {text}
       </Text>
     </CustomCard>
