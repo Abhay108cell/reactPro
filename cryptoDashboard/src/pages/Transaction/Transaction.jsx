541d6280e7bb8525b0f19047a9bb5dde5dcd3570
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-09 17:30:51 +0530

    "Added Chakra UI Tabs component and modified Export CSV button with icon"

--- cryptoDashboard/src/pages/Transaction/Transaction.jsx
+++ cryptoDashboard/src/pages/Transaction/Transaction.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import DashboardLayout from '../../components/DashboardLayout'
 import {
   Stack,
@@ -10,15 +11,34 @@ import {
   Button,
   Card
 } from "@chakra-ui/react";
+import { TiDownload } from 'react-icons/ti';
 
 const TransactionPage = () => {
   return (
     <DashboardLayout>
-      <Flex justify="end" mt="6">
-        <Button>Export CSV</Button>
+      <Flex justify="end" mt="6" mb="3">
+        <Button leftIcon={<Icon as={TiDownload}/>}>Export CSV</Button>
       </Flex>
       <Card>
-      transaction page
+      <Tabs>
+  <TabList>
+    <Tab>One</Tab>
+    <Tab>Two</Tab>
+    <Tab>Three</Tab>
+  </TabList>
+
+  <TabPanels>
+    <TabPanel>
+      <p>one!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>three!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
     </Card>
     </DashboardLayout>
   )
