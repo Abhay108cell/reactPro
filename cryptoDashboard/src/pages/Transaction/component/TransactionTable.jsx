22f7e92134de476c71633487c184f4f9bd5b89b4
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-10 16:26:59 +0530

    "Updated TransactionTable component to display custom table data with modified columns and added Stack component from Chakra UI."

--- cryptoDashboard/src/pages/Transaction/component/TransactionTable.jsx
+++ cryptoDashboard/src/pages/Transaction/component/TransactionTable.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 import {
   Table,
   Thead,
   Tbody,
   Tfoot,
+  Stack,
   Tr,
   Th,
   Td,
   TableCaption,
   TableContainer,
-} from '@chakra-ui/react'
+  Text,
+} from "@chakra-ui/react";
 
 const TransactionTable = () => {
   const tableData = [
@@ -79,42 +81,40 @@ const TransactionTable = () => {
     },
   ];
   return (
-<TableContainer>
-  <Table variant='simple'>
-    <TableCaption>Imperial to metric conversion factors</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>inches</Td>
-        <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
-      </Tr>
-      <Tr>
-        <Td>feet</Td>
-        <Td>centimetres (cm)</Td>
-        <Td isNumeric>30.48</Td>
-      </Tr>
-      <Tr>
-        <Td>yards</Td>
-        <Td>metres (m)</Td>
-        <Td isNumeric>0.91444</Td>
-      </Tr>
-    </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Tfoot>
-  </Table>
-</TableContainer>  )
-}
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Date & Time</Th>
+            <Th>Type</Th>
+            <Th>Amount</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {tableData.map((data)=>(
+            <Tr key={data.id}>
+              <Td>
+                {data.id}
+              </Td>
+              <Td>
+                <Stack>
+                  <Text>{data.date}</Text>
+                  <Text>{data.time}</Text>
+                </Stack>
+              </Td>
+              <Td>{data.id}</Td>
+              <Td>{data.id}</Td>
+              <Td>{data.id}</Td>
+              <Td>{data.id}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-export default TransactionTable
+export default TransactionTable;
