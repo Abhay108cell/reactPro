65c8d288c22cc517dd76f4b6334c330b0e82ede4
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-11 11:20:45 +0530

    "Added Icon from react icon and Text components to SupportCard component"

--- cryptoDashboard/src/pages/Support/component/SupportCard.jsx
+++ cryptoDashboard/src/pages/Support/component/SupportCard.jsx
@@ -1,11 +1,13 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Icon, Text } from '@chakra-ui/react'
 import React from 'react'
+import { IoMail } from 'react-icons/io5'
 
 const SupportCard = () => {
   return (
     <Flex>
       <Stack>
-        
+        <Icon as={IoMail}/>
+        <Text textStyle="h2" >Contact Us</Text>
       </Stack>
     </Flex>
   )
