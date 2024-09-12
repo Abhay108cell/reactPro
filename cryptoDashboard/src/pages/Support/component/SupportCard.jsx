eb4c9ed4a1e2bf418bb6b6d5a8964c0be82cb9c3
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-12 17:20:25 +0530

    "Updated SupportCard component with new imports, formatting changes, and added form elements"

--- cryptoDashboard/src/pages/Support/component/SupportCard.jsx
+++ cryptoDashboard/src/pages/Support/component/SupportCard.jsx
@@ -1,22 +1,43 @@
-import { Flex, Icon, Text,Stack } from '@chakra-ui/react'
-import React from 'react'
-import { IoMail } from 'react-icons/io5'
+import {
+  Flex,
+  Icon,
+  Text,
+  Stack,
+  Card,
+  HStack,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import React from "react";
+import { IoMail } from "react-icons/io5";
 
 const SupportCard = () => {
   return (
     <Flex>
       <Stack>
-        <Icon as={IoMail} boxSize={6} color="p.purple"/>
-        <Text fontWeight="sm" as="h1" textStyle="h2" >Contact Us</Text>
-        <Text fontSize="sm" color="black.60" >Have a question or just want to know more? Feel free to reach out to us.</Text>
-      </Stack>
-      <Card p="6" borderRadius="1rem" >
-        <Text fontWeight="medium" fontSize="sm">
-        You will receive response within 24 hours of time of submit.
+        <Icon as={IoMail} boxSize={6} color="p.purple" />
+        <Text fontWeight="sm" as="h1" textStyle="h2">
+          Contact Us
         </Text>
+        <Text fontSize="sm" color="black.60">
+          Have a question or just want to know more? Feel free to reach out to
+          us.
+        </Text>
+      </Stack>
+      <Card p="6" borderRadius="1rem">
+        <Text fontWeight="medium" fontSize="sm">
+          You will receive response within 24 hours of time of submit.
+        </Text>
+        <HStack>
+          <FormControl>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" />
+          </FormControl>
+        </HStack>
       </Card>
     </Flex>
-  ) 
-}
+  );
+};
 
-export default SupportCard
+export default SupportCard;
