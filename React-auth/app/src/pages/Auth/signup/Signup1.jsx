47f57d1b6d4c768a93d855a80935dcdb51992087
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-14 16:33:38 +0530

    "Updated Signup1 component: added Text import, changed Card padding and width, and added Text element with heading"

--- React-auth/app/src/pages/Auth/signup/Signup1.jsx
+++ React-auth/app/src/pages/Auth/signup/Signup1.jsx
@@ -1,12 +1,12 @@
-import { Card, Container, Center } from "@chakra-ui/react";
+import { Card, Container, Center, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Signup1 = () => {
   return (
     <Container>
-      <Center>
-        <Card p="4" borderRadius="1rem" maxW="408px">
-          Signup1
+      <Center minHeight="100vh">
+        <Card p="6" borderRadius="1rem" W="408px">
+          <Text textStyle="h1">Welcome to Crypto App</Text>
         </Card>
       </Center>
     </Container>
