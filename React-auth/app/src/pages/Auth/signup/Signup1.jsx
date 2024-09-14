7b30a6cdae9c9093f3bae1ca2f0912ed04a2173f
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-14 17:19:45 +0530

    "Updated Signup1 component with additional Chakra UI import and modified layout"

--- React-auth/app/src/pages/Auth/signup/Signup1.jsx
+++ React-auth/app/src/pages/Auth/signup/Signup1.jsx
@@ -1,4 +1,14 @@
-import { Card, Container, Center, Text, Stack, FormControl, Input, FormLabel } from "@chakra-ui/react";
+import {
+  Card,
+  Container,
+  Center,
+  Text,
+  Stack,
+  FormControl,
+  Input,
+  FormLabel,
+  Flex,
+} from "@chakra-ui/react";
 import React from "react";
 
 const Signup1 = () => {
@@ -6,17 +16,21 @@ const Signup1 = () => {
     <Container>
       <Center minHeight="100vh">
         <Card p="6" borderRadius="1rem" W="456px">
-          <Text textStyle="h1" >Welcome to Crypto App</Text>
-          <Text textStyle="p2" color="black.60" mt="4">Create a free account by filling data below.</Text>
-          <Stack>
-            <FormControl>
-              <FormLabel htmlFor="name">Name</FormLabel>
-              <Input for="name" placeholder="Enter your name..."/>
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="surname">surname</FormLabel>
-              <Input for="surname" placeholder="Enter your surname..."/>
-            </FormControl>
+          <Text textStyle="h1">Welcome to Crypto App</Text>
+          <Text textStyle="p2" color="black.60" mt="4">
+            Create a free account by filling data below.
+          </Text>
+          <Stack mt="10">
+            <Flex gap="4">
+              <FormControl>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <Input for="name" placeholder="Enter your name..." />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="surname">surname</FormLabel>
+                <Input for="surname" placeholder="Enter your surname..." />
+              </FormControl>
+            </Flex>
           </Stack>
         </Card>
       </Center>
