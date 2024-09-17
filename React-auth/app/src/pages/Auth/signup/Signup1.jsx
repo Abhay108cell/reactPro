bbe6e555df7fd9c480037342dca36292fa9b046e
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-17 18:11:18 +0530

    "Refactored Signup1 component to wrap form fields with Formik's Form component and added arrow function to Formik component."

--- React-auth/app/src/pages/Auth/signup/Signup1.jsx
+++ React-auth/app/src/pages/Auth/signup/Signup1.jsx
@@ -14,8 +14,7 @@ import {
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Formik, Form, Field } from 'formik';
-
+import { Formik, Form, Field } from "formik";
 
 const Signup1 = () => {
   return (
@@ -27,42 +26,66 @@ const Signup1 = () => {
             Create a free account by filling data below.
           </Text>
           <Formik>
-          <Stack mt="10" spacing={6}>
-            <Flex gap="4">
-              <FormControl>
-                <FormLabel htmlFor="name">Name</FormLabel>
-                <Input for="name" placeholder="Enter your name..." />
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="surname">surname</FormLabel>
-                <Input for="surname" placeholder="Enter your surname..." />
-              </FormControl>
-            </Flex>
-            <FormControl>
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input for="email" type="email" placeholder="Enter your email..." />
-              </FormControl>
-            <FormControl>
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <Input for="password" type="password" placeholder="Enter your password..." />
-              </FormControl>
-            <FormControl>
-                <FormLabel htmlFor="repeatPassword">Repeat Password</FormLabel>
-                <Input for="repeatPassword" type="password" placeholder="Enter your repeat Password..." />
-              </FormControl>
-              <Checkbox>
-              <Text textStyle="p3">
-              I agree with <Text as="span" color="p.purple">
-                  Terms and Conditions
-                </Text>
-              </Text>
-              </Checkbox>
-              <Button type="submit">Create Account</Button>
-              <Text textStyle="p3" color="black.60" textAlign="center">Already have an account ? <Link to="/signin"><Text as="span" color="p.purple">Log In</Text></Link></Text>
-          </Stack>
-            </Form>)}
-            
-          
+            {()=>(
+                 <Form>
+                 <Stack mt="10" spacing={6}>
+                   <Flex gap="4">
+                     <FormControl>
+                       <FormLabel htmlFor="name">Name</FormLabel>
+                       <Input for="name" placeholder="Enter your name..." />
+                     </FormControl>
+                     <FormControl>
+                       <FormLabel htmlFor="surname">surname</FormLabel>
+                       <Input for="surname" placeholder="Enter your surname..." />
+                     </FormControl>
+                   </Flex>
+                   <FormControl>
+                     <FormLabel htmlFor="email">Email</FormLabel>
+                     <Input
+                       for="email"
+                       type="email"
+                       placeholder="Enter your email..."
+                     />
+                   </FormControl>
+                   <FormControl>
+                     <FormLabel htmlFor="password">Password</FormLabel>
+                     <Input
+                       for="password"
+                       type="password"
+                       placeholder="Enter your password..."
+                     />
+                   </FormControl>
+                   <FormControl>
+                     <FormLabel htmlFor="repeatPassword">
+                       Repeat Password
+                     </FormLabel>
+                     <Input
+                       for="repeatPassword"
+                       type="password"
+                       placeholder="Enter your repeat Password..."
+                     />
+                   </FormControl>
+                   <Checkbox>
+                     <Text textStyle="p3">
+                       I agree with{" "}
+                       <Text as="span" color="p.purple">
+                         Terms and Conditions
+                       </Text>
+                     </Text>
+                   </Checkbox>
+                   <Button type="submit">Create Account</Button>
+                   <Text textStyle="p3" color="black.60" textAlign="center">
+                     Already have an account ?{" "}
+                     <Link to="/signin">
+                       <Text as="span" color="p.purple">
+                         Log In
+                       </Text>
+                     </Link>
+                   </Text>
+                 </Stack>
+               </Form>
+            )}
+           
           </Formik>
         </Card>
       </Center>
