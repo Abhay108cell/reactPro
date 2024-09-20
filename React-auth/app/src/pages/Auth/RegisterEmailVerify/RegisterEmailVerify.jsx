f7e730648f544252889c2b5d06ad79cedf399d7f
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-20 16:10:04 +0530

    "Added RegisterEmailVerify page to App.jsx and created RegisterEmailVerify component with email verification UI"

--- React-auth/app/src/pages/Auth/RegisterEmailVerify/RegisterEmailVerify.jsx
+++ React-auth/app/src/pages/Auth/RegisterEmailVerify/RegisterEmailVerify.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import Card from '../../../components/Card'
-import { Icon, VStack } from '@chakra-ui/react'
+import React from "react";
+import Card from "../../../components/Card";
+import { Icon, Text, VStack } from "@chakra-ui/react";
 import { MdEmail } from "react-icons/md";
-
 
 const RegisterEmailVerify = () => {
   return (
     <Card>
       <VStack>
-      <Icon as={MdEmail} boxSize="6" color="p.purple"/>
+        <Icon as={MdEmail} boxSize="48px" color="p.purple" />
+        <Text textStyle="h4" color="p.black">
+          Email Verification
+        </Text>
+        <Text textStyle="p2" color="p.black">
+          We have sent you an email verification abhay@gmail . If you didn’t
+          receive it, click the button below.
+        </Text>
       </VStack>
     </Card>
-  )
-}
+  );
+};
 
-export default RegisterEmailVerify
+export default RegisterEmailVerify;
