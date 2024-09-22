3f5d00f826cdec864e545b12d4cf2f09c0d7bb4c
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-22 20:03:54 +0530

    "Wrapped Center components with Container in ForgotPasswordSend and RegisterSuccess pages"

--- React-auth/app/src/pages/Auth/ForgotPasswordSend/ForgotPasswordSend.jsx
+++ React-auth/app/src/pages/Auth/ForgotPasswordSend/ForgotPasswordSend.jsx
@@ -1,31 +1,44 @@
 import React from "react";
 import Card from "../../../components/Card";
-import { Box, Button, Center, Icon, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Icon,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { MdEmail } from "react-icons/md";
 
 const ForgotPasswordSend = () => {
   return (
-   <Center minH="100vh">
-     <Card
-      p={{
-        base: "4",
-        md: "10",
-      }} 
-      showCard={true}
-     >
-      <VStack spacing={6}>
-        <Icon as={MdEmail} boxSize="48px" color="p.purple" />
-        <Text  textStyle="h4" color="p.black" fontWeight="medium">
-         Successfully Sent
-        </Text>
-        <Text textAlign="center" textStyle="p2" color="black.60">
-          We have sent you an email verification <Box as="b" color="p.black">abhay@gmail.com</Box> Please follow the instructions from the email. We have sent you an
-          email verification to{" "}
-        </Text>
-        
-      </VStack>
-    </Card>
-   </Center>
+    <Container>
+      <Center minH="100vh">
+        <Card
+          p={{
+            base: "4",
+            md: "10",
+          }}
+          showCard={true}
+        >
+          <VStack spacing={6}>
+            <Icon as={MdEmail} boxSize="48px" color="p.purple" />
+            <Text textStyle="h4" color="p.black" fontWeight="medium">
+              Successfully Sent
+            </Text>
+            <Text textAlign="center" textStyle="p2" color="black.60">
+              We have sent you an email verification{" "}
+              <Box as="b" color="p.black">
+                abhay@gmail.com
+              </Box>{" "}
+              Please follow the instructions from the email. We have sent you an
+              email verification to{" "}
+            </Text>
+          </VStack>
+        </Card>
+      </Center>
+    </Container>
   );
 };
 
--- React-auth/app/src/pages/Auth/RegisterSuccess/RegisterSuccess.jsx
+++ React-auth/app/src/pages/Auth/RegisterSuccess/RegisterSuccess.jsx
@@ -1,38 +1,46 @@
 import React from "react";
 import Card from "../../../components/Card";
-import { Box, Button, Center, Icon, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Icon,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { BsPatchCheckFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const RegisterSuccess = () => {
   return (
-   <Center minH="100vh">
-     <Card
-       
-       p={{
-         base: "4",
-         md: "10",
-       }} 
-       showCard={true}
-      >
-     
-      <VStack spacing={6}>
-        <Icon as={BsPatchCheckFill} boxSize="48px" color="green" />
-        <Text  textStyle="h4" color="p.black" fontWeight="medium">
-        Successfully Registration
-        </Text>
-        <Text textAlign="center" textStyle="p2" color="black.60">
-        Hurray! You have successfully created your account. Enter the app to explore all it’s features.
-        </Text>
-       <Box w="full">
-       <Link to="/signin">
-        <Button w="full" >Enter the app</Button>
-        </Link>
-       </Box>
-       
-      </VStack>
-    </Card>
-   </Center>
+    <Container>
+      <Center minH="100vh">
+        <Card
+          p={{
+            base: "4",
+            md: "10",
+          }}
+          showCard={true}
+        >
+          <VStack spacing={6}>
+            <Icon as={BsPatchCheckFill} boxSize="48px" color="green" />
+            <Text textStyle="h4" color="p.black" fontWeight="medium">
+              Successfully Registration
+            </Text>
+            <Text textAlign="center" textStyle="p2" color="black.60">
+              Hurray! You have successfully created your account. Enter the app
+              to explore all it’s features.
+            </Text>
+            <Box w="full">
+              <Link to="/signin">
+                <Button w="full">Enter the app</Button>
+              </Link>
+            </Box>
+          </VStack>
+        </Card>
+      </Center>
+    </Container>
   );
 };
 
