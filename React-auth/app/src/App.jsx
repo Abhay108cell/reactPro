bb20b28c2747359d722938e0df9b664cc4951e49
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-23 15:52:28 +0530

    "Updated package-lock.json and package.json to include @tanstack/react-query-devtools, and modified App.jsx to import QueryClient and QueryClientProvider from @tanstack/react-query."

--- React-auth/app/src/App.jsx
+++ React-auth/app/src/App.jsx
@@ -11,66 +11,64 @@ import ForgotPassword from "./pages/Auth/ForgotPassword/ForgotPassword";
 import ForgotPasswordSend from "./pages/Auth/ForgotPasswordSend/ForgotPasswordSend";
 import ResetPasswordSuccess from "./pages/Auth/ResetPasswordSuccess/ForgotPasswordSend";
 import ResetPassword from "./pages/Auth/ResetPassword/ResetPassword";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/transactions",
-    element: <TransactionPage />,
-  },
-  {
-    path: "/support",
-    element: <Support />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/signin",
-    element: <Signin />,
-  },
-  {
-    path: "/Register-Email-Verify",
-    element: <RegisterEmailVerify />,
-  },
-  {
-    path: "/Register-Success",
-    element: <RegisterSuccess />,
-  },
-  {
-    path: "/Forgot-Password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/Forgot-Password-send",
-    element: <ForgotPasswordSend />,
-  },
-  {
-    path: "/Reset-password",
-    element: <ResetPassword />,
-  },
-  {
-    path: "/Reset-Success",
-    element: <ResetPasswordSuccess />,
-  },
-]);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
-  const queryClient = new QueryClient()
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "/transactions",
+      element: <TransactionPage />,
+    },
+    {
+      path: "/support",
+      element: <Support />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/signin",
+      element: <Signin />,
+    },
+    {
+      path: "/Register-Email-Verify",
+      element: <RegisterEmailVerify />,
+    },
+    {
+      path: "/Register-Success",
+      element: <RegisterSuccess />,
+    },
+    {
+      path: "/Forgot-Password",
+      element: <ForgotPassword />,
+    },
+    {
+      path: "/Forgot-Password-send",
+      element: <ForgotPasswordSend />,
+    },
+    {
+      path: "/Reset-password",
+      element: <ResetPassword />,
+    },
+    {
+      path: "/Reset-Success",
+      element: <ResetPasswordSuccess />,
+    },
+  ]);
+
+  const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
