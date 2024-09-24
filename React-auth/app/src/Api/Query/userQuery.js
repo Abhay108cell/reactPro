1f73e6a108fc7b9bed62ca54d14763b7ada0fe45
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-09-24 18:37:27 +0530

    "Added signinUser function to userQuery.js and modified Dashboard.jsx to rename useQuery instance to 'example'"

---
+++ React-auth/app/src/Api/Query/userQuery.js
@@ -0,0 +1,11 @@
+const USER_URL = "/user"
+
+
+export const signinUser = async ({password, email}) =>{
+  try {
+    const data = Axios.post(`${USER_URL}/signin`)
+    return data
+  } catch (error) {
+    throw Error(error.response.data.message)
+  }
+}
