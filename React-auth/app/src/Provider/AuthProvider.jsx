5fd1c5c6e72786688f80fb74fb70309d7c26e3f4
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-02 16:43:26 +0530

    "Added jwt-decode and react-cookie dependencies, and updated AuthProvider to use useState for user and token."

--- React-auth/app/src/Provider/AuthProvider.jsx
+++ React-auth/app/src/Provider/AuthProvider.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { createContext } from "react";
 
 export const AuthContext = createContext();
 
 export  default function AuthProvider({children}){
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
   return(
     <AuthContext.Provider
     value={{
