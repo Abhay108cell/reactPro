3273bdc6dfb4d2b26f094c8d140fc153d7d2df4b
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-02 16:33:20 +0530

    "Updated AuthProvider component and wrapped App with AuthProvider in main.jsx"

--- React-auth/app/src/Provider/AuthProvider.jsx
+++ React-auth/app/src/Provider/AuthProvider.jsx
@@ -3,14 +3,16 @@ import { createContext } from "react";
 export const AuthContext = createContext();
 
 export  default function AuthProvider({children}){
-  return <AuthContext.Provider
-  value={{
-    user,
-    token,
-    login,
-    logout,
-  }}
->
-  {children}
-</AuthContext.Provider>
+  return(
+    <AuthContext.Provider
+    value={{
+      user,
+      token,
+      login,
+      logout,
+    }}
+  >
+    {children}
+  </AuthContext.Provider>
+  )
 }
