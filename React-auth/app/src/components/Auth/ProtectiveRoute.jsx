ded435fb735fc2a2473ada8fde370f968b1636b0
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-04 17:13:39 +0530

    "Updated ProtectiveRoute component to use token from useAuth hook and redirect to signin page if token is not present"

--- React-auth/app/src/components/Auth/ProtectiveRoute.jsx
+++ React-auth/app/src/components/Auth/ProtectiveRoute.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import useAuth from '../../hookes/useAuth'
+import { Navigate } from 'react-router-dom';
 
 const ProtectiveRoute = ({children}) => {
-  const {} = useAuth();
+  const {token} = useAuth();
 
-  return (
-    <div>ProtectiveRoute</div>
-  )
+  return <>{token ? children : <Navigate to="/signin" />}</>
+
 }
 
 export default ProtectiveRoute
