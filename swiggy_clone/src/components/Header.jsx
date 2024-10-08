f4dd1f4729811fce209d7e8d7ce94b364ae86339
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-08 18:38:23 +0530

    "Updated Header component: added conditional styling to black-overlay div, added onClick event to RxCaretDown icon"

--- swiggy_clone/src/components/Header.jsx
+++ swiggy_clone/src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   const [toggle, setToggle] = useState(false)
   return (
    <>
-   <div className="black-overlay"></div>
+   <div className="black-overlay w-full h-full fixed duration-500" style={{
+    opacity: toggle ?  1 : 0
+   }}></div>
     <header className='p-2 shadow-xl'>
       <div className="max-w-[1200px] mx-auto border border-red-500 flex items-center">
        <div className='w-[100px]'>
@@ -13,7 +15,7 @@ const Header = () => {
        </div>
        <div className="">
         <span className='font-bold border-b-[3px] border-[black] '> Meerut</span>
-         Hapur, India <RxCaretDown fontSize={25} className='font-bold inline  text-[#fc8019]'  />
+         Hapur, India <RxCaretDown onClick={showSideMenu} fontSize={25} className='font-bold inline  text-[#fc8019]'  />
        </div>
       </div>
     </header>
