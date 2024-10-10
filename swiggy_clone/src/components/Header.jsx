c0648627cf426a65fb9d0a79d3d1639ec6a34f43
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-10 17:53:02 +0530

    "Updated Header component: added font-bold and text-[18px] styles to nav element"

--- swiggy_clone/src/components/Header.jsx
+++ swiggy_clone/src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
         <span className='font-bold border-b-[3px] border-[black] '> Meerut</span>
          Hapur, India <RxCaretDown onClick={showSideMenu} fontSize={25} className='font-bold inline  text-[#fc8019] cursor-pointer'  />
        </div>
-       <nav className='flex list-none gap-4 ml-auto'>
+       <nav className='flex list-none gap-4 ml-auto font-bold text-[18px]'>
         <li>
           Search
         </li>
