253482f871a9f3bd8da7754054451f8e206d6ef6
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-11 22:31:58 +0530

    "Updated Category component: changed inner div to flex container and added rounded-full and mx-2 classes to inner divs."

--- swiggy_clone/src/components/Category.jsx
+++ swiggy_clone/src/components/Category.jsx
@@ -6,9 +6,9 @@ const Category = () => {
     <div className="max-w-[1200px] mx-auto  flex items-center">
     <div>
     <div>What's on your mind?</div>
-    <div>
-      <div className='w-[30px] h-[30px] bg-[#e2e2e7] '></div>
-      <div className='w-[30px] h-[30px] bg-[#e2e2e7] '></div>
+    <div className='flex'>
+      <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 '></div>
+      <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
     </div>
     </div>
     </div>
