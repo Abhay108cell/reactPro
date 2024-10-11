6263141014d104e946feca0333692513cbc15ac8
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-11 22:30:01 +0530

    "Added container div with max-width and updated inner divs with background color"

--- swiggy_clone/src/components/Category.jsx
+++ swiggy_clone/src/components/Category.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const Category = () => {
   return (
     <>
+    <div className="max-w-[1200px] mx-auto  flex items-center">
     <div>
     <div>What's on your mind?</div>
     <div>
-      <div className='w-[30px] h-[30px]'></div>
+      <div className='w-[30px] h-[30px] bg-[#e2e2e7] '></div>
+      <div className='w-[30px] h-[30px] bg-[#e2e2e7] '></div>
+    </div>
     </div>
     </div>
         
