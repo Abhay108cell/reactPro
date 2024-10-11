9e77e39b1f9227d3644c9fee67d6fc7c294112ad
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-11 22:39:28 +0530

    "Updated Category component: changed font size and style of category title, replaced empty divs with react-icons FaArrowLeft and FaArrowRight"

--- swiggy_clone/src/components/Category.jsx
+++ swiggy_clone/src/components/Category.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 const Category = () => {
   return (
     <>
     <div className="max-w-[1200px] mx-auto  ">
     <div className='flex my-3 items-center justify-between'>
-    <div className='text-[20px]'>What's on your mind?</div>
+    <div className='text-[25px] font-bold'>What's on your mind?</div>
     <div className='flex'>
-      <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 '></div>
-      <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
+      <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 '><FaArrowLeft/></div>
+      <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'><FaArrowRight/></div>
     </div>
     </div>
     </div>
