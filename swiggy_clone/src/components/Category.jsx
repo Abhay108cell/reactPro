df45a73f9ff3d0b167c5d821acccfc7cbd474689
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-11 22:37:04 +0530

    "Updated Category component: added 'my-3' class to div, changed text size to 20px, and reformatted HTML structure."

--- swiggy_clone/src/components/Category.jsx
+++ swiggy_clone/src/components/Category.jsx
@@ -4,8 +4,8 @@ const Category = () => {
   return (
     <>
     <div className="max-w-[1200px] mx-auto  ">
-    <div className='flex items-center justify-between'>
-    <div>What's on your mind?</div>
+    <div className='flex my-3 items-center justify-between'>
+    <div className='text-[20px]'>What's on your mind?</div>
     <div className='flex'>
       <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 '></div>
       <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
