ee20725fcb7f97e61a744bbca800f13516c1e2dd
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-13 21:16:41 +0530

    "Updated Category component to fetch and set categories from API"

--- swiggy_clone/src/components/Category.jsx
+++ swiggy_clone/src/components/Category.jsx
@@ -4,7 +4,9 @@ import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 const Category = () => {
   const [categories, setCategory] = useState([]);
   const fetchCategory = async () =>{
-    const response =  await fetch('http://localhost:5000/categories')
+    const response =  await fetch('http://localhost:5000/categories');
+    const data = await response.json();
+    setCategory(data);
   }
     useEffect(
       ()=>{
