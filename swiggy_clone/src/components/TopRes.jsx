e71555083f1031b039ab254f0f2fa81b7c3bb2a7
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-14 18:57:52 +0530

    "Added API data parsing and rendering, and included Card component in TopRes component"

--- swiggy_clone/src/components/TopRes.jsx
+++ swiggy_clone/src/components/TopRes.jsx
@@ -6,7 +6,8 @@ export default function TopRes() {
 
   const fetchTopRestaurant = async () => {
     const response = await fetch('http://localhost:5000/top-restaurant-chains')
-    
+    const apiData = await response.json()
+    setData(apiData)
   }
 
   useEffect(
@@ -31,6 +32,9 @@ export default function TopRes() {
           <FaArrowRight  />
         </div>
       </div>
+    </div>
+    <div className="flex">
+      <Card/>
     </div>
     </div>
   )
