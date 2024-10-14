ab45dfc87ae8ad434199b5ee95ae5dc46002234f
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-14 19:06:10 +0530

    "Added Card component with image and overlay"

---
+++ swiggy_clone/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+   <div className="w-[273px]">
+    <div className='h-[182px] rounded-[15px] overflow-hidden relative' >
+      <img className='object-cover w-full h-full' src="https://picsum.photos/200/300" alt="image" />
+      <div className="image-overlay w-full h-full"></div>
+    </div>
+   </div>
+  )
+}
