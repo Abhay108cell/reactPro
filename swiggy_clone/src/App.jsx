28cc1a62539b5b10e794278e86989d2a155a265d
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-14 17:43:00 +0530

    "Added TopRes component to App and implemented its UI with navigation buttons"

--- swiggy_clone/src/App.jsx
+++ swiggy_clone/src/App.jsx
@@ -1,12 +1,14 @@
 import "./index.css"; // Or the path to your CSS file
 import Header from "./components/Header";
 import Category from "./components/Category";
+import TopRes from "./components/TopRes";
 
 function App() {
   return (
     <>
       <Header />
       <Category/>
+      <TopRes/>
     </>
   );
 }
