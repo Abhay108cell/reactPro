2520ff4a3ffd3868794482886b2192783a7a01fe
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-15 15:15:19 +0530

    "Updated Card component styles and added multiple Card components to TopRes component"

--- swiggy_clone/src/components/Card.jsx
+++ swiggy_clone/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card() {
   return (
-   <div className="w-[273px]">
+   <div className="w-[273px] shrink-0 grow">
     <div className='h-[182px] rounded-[15px] overflow-hidden relative' >
       <img className='object-cover w-full h-full' src="https://picsum.photos/200/300" alt="image" />
       <div className="image-overlay absolute w-full h-full top-0 flex items-end p-2 text-[25px] font bold text-white tracking-tighter">
