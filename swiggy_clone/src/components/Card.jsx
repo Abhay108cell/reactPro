2a4b99c3dc9ac31f2762799a95abe6d066cf8dbc
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-15 15:13:00 +0530

    "Updated Card component with new image overlay design and styling"

--- swiggy_clone/src/components/Card.jsx
+++ swiggy_clone/src/components/Card.jsx
@@ -5,7 +5,9 @@ export default function Card() {
    <div className="w-[273px]">
     <div className='h-[182px] rounded-[15px] overflow-hidden relative' >
       <img className='object-cover w-full h-full' src="https://picsum.photos/200/300" alt="image" />
-      <div className="image-overlay absolute w-full h-full"></div>
+      <div className="image-overlay absolute w-full h-full top-0 flex items-end p-2 text-[25px] font bold text-white tracking-tighter">
+        Item at $179
+      </div>
     </div>
    </div>
   )
