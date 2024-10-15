b66e93362c2e1117a3350609ea776c3df8adec04
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-15 14:56:07 +0530

    "Updated Card component to add 'absolute' class to image overlay, and imported Card component in TopRes component."

--- swiggy_clone/src/components/Card.jsx
+++ swiggy_clone/src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card() {
    <div className="w-[273px]">
     <div className='h-[182px] rounded-[15px] overflow-hidden relative' >
       <img className='object-cover w-full h-full' src="https://picsum.photos/200/300" alt="image" />
-      <div className="image-overlay w-full h-full"></div>
+      <div className="image-overlay absolute w-full h-full"></div>
     </div>
    </div>
   )
