9d527bae9c3b2118f0f4671fe4203409a5776c63
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-16 18:18:07 +0530

    "Updated OnlineDelivery component with added imports, modified fetchTopRestaurant function, and added HTML elements with classes"

--- swiggy_clone/src/components/OnlineDelivery.jsx
+++ swiggy_clone/src/components/OnlineDelivery.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function OnlineDelivery() {
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
 
   const fetchTopRestaurant = async () => {
-    const response = await fetch('http://localhost:5000/top-restaurant-chains')
-    const apiData = await response.json()
-    setData(apiData)
-  }
+    const response = await fetch("http://localhost:5000/top-restaurant-chains");
+    const apiData = await response.json();
+    setData(apiData);
+  };
 
-  useEffect(
-    () => {
-      fetchTopRestaurant()
-    },[]
-  )
-
+  useEffect(() => {
+    fetchTopRestaurant();
+  }, []);
 
   return (
-    <div>
-      
+    <div className="max-w-[1200px] mx-auto  mb-[100px] ">
+      <div className="flex my-3 items-center justify-between">
+        <div className="text-[25px] font-bold">
+          Top Restaurant chains in Meerut
+        </div>
+      </div>
     </div>
-  )
+  );
 }
