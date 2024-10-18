3d4f898c76d33b9f427194bad1fc0e21d6aadd1e
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-18 15:16:58 +0530

    "Updated styles for Card and OnlineDelivery components"

--- swiggy_clone/src/components/Card.jsx
+++ swiggy_clone/src/components/Card.jsx
@@ -5,11 +5,11 @@ export default function Card(props) {
    <div className={`${props.width} shrink-0 mb-3`}>
     <div className='group h-[182px] rounded-[15px] overflow-hidden relative' >
       <img className='group-hover:scale-110 duration-100 object-cover w-full h-full' src={"https://picsum.photos/200/300"+props.image} alt="image" />
-      <div className="image-overlay absolute w-full h-full top-0 flex items-end p-2 text-[25px] font bold text-white tracking-tighter">
+      <div className="image-overlay absolute w-full h-full top-0 flex items-end p-2 text-[16px] md:text-[25px] font bold text-white tracking-tighter">
         {props.offer}
       </div>
     </div>
-      <div className='mt-3 text-xl font-bold' >
+      <div className='mt-3 text:md md:text-xl font-bold' >
         {props.title}
       </div>
       <div>
